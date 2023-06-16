2d29d9172846016c1c4aeee4f540cffeb7b71b5d
lehoanganh298 <lehoanganh29896@gmail.com>
2023-06-16 11:24:47 +0000

    Add Zing Quiz and Kiki QAC dataset (cont)

---
+++ lm_eval/tasks/kiki_qac.py
@@ -0,0 +1,84 @@
+# TODO: Remove all TODO comments once the implementation is complete.
+"""
+TODO: Add the Paper Title on this line.
+TODO: Add the paper's PDF URL (preferably from arXiv) on this line.
+
+TODO: Write a Short Description of the task.
+
+Homepage: TODO: Add the URL to the task's Homepage here.
+"""
+import json
+import datasets
+from lm_eval.base import MultipleChoiceTask
+
+
+# TODO: Add the BibTeX citation for the task.
+_CITATION = """
+"""
+
+
+# TODO: Replace `NewTask` with the name of your Task.
+class Kiki_QAC(MultipleChoiceTask):
+    VERSION = 0
+    # TODO: Add the `DATASET_PATH` string. This will be the name of the `Task`
+    # dataset as denoted in HuggingFace `datasets`.
+    DATASET_PATH = ""
+    with open('/home/qac_test.jsonl') as f:
+        data = [json.loads(line) for line in f]
+    DATASET_OBJ = {'test': data}
+
+    # TODO: Add the `DATASET_NAME` string. This is the name of a subset within
+    # `DATASET_PATH`. If there aren't specific subsets you need, leave this as `None`.
+    DATASET_NAME = None
+
+    def has_training_docs(self):
+        # TODO: Fill in the return with `True` if the Task has training data; else `False`.
+        return False
+
+    def has_validation_docs(self):
+        # TODO: Fill in the return with `True` if the Task has validation data; else `False`.
+        return False
+
+    def has_test_docs(self):
+        # TODO: Fill in the return with `True` if the Task has test data; else `False`.
+        return True
+
+    def training_docs(self):
+        if self.has_training_docs():
+            # We cache training documents in `self._training_docs` for faster
+            # few-shot processing. If the data is too large to fit in memory,
+            # return the training data as a generator instead of a list.
+            if self._training_docs is None:
+                # TODO: Return the training document generator from `self.dataset`.
+                # In most case you can leave this as is unless the dataset split is
+                # named differently than the default `"train"`.
+                self._training_docs = list(
+                    map(self._process_doc, self.dataset["train"])
+                )
+            return self._training_docs
+
+    def validation_docs(self):
+        if self.has_validation_docs():
+            # TODO: Return the validation document generator from `self.dataset`.
+            # In most case you can leave this as is unless the dataset split is
+            # named differently than the default `"validation"`.
+            return map(self._process_doc, self.dataset["validation"])
+
+    def test_docs(self):
+        if self.has_test_docs():
+            # TODO: Return the test document generator from `self.dataset`.
+            # In most case you can leave this as is unless the dataset split is
+            # named differently than the default `"test"`.
+            return map(self._process_doc, self.dataset["test"])
+
+    def _process_doc(self, doc):
+        # TODO: Process the documents into a dictionary with the following keys:
+        return {
+            "query": f"Cho đoạn văn sau: {doc['text']}. Đoạn văn trên có chứa câu trả lời cho câu hỏi \"{doc['question']}\" không?\nCâu trả lời:",
+            "choices": ["Có chứa câu trả lời", "Không chứa câu trả lời"],
+            "gold": 0 if doc['label'] else 1,
+        }
+
+    def doc_to_text(self, doc):
+        # TODO: Format the query prompt portion of the document example.
+        return doc["query"]
